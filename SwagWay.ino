b0bf8c0fd5584eb09f3f9ec779b154d1769362f0
Sjoer van der Ploeg <sfjuocekr@gmail.com>
2017-01-09 16:23:23 +0100

    Complete overhaul!
    Added:
    
    - braking;
    - "self centering" when possible;
    - other mumbo jumbo!

--- SwagWay.ino
+++ SwagWay.ino
@@ -1,51 +1,50 @@
+#include <Thread.h>
+#include <ThreadController.h>
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
-
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
-#endif
 
-#define ENA 3
+
+ThreadController mainThread = ThreadController();
+
+Thread* outputThread = new Thread();
+Thread* sensorThread = new Thread();
+Thread* motorThread = new Thread();
+
+
+/* L298N pins, ENA/ENB must be hardware PWM outputs */
+#define ENA 9
 #define IN1 4
 #define IN2 5
 
-#define ENB 6
-#define IN3 7
-#define IN4 8
+#define ENB 10
+#define IN3 6
+#define IN4 7
+
 
 MPU6050 mpu;
 
-//#define OUTPUT_READABLE_YAWPITCHROLL
-#define OUTPUT_READABLE_MOTOR
-//#define OUTPUT_TEAPOT
 
 #define INTERRUPT_PIN 2
-#define LED_PIN 13
-
-bool blinkState = false;
-
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-// packet structure for InvenSense "teapot" demo
-uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
 
 
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
+volatile bool mpuInterrupt = false;
+uint8_t mpuIntStatus;
+uint16_t packetSize;
+uint16_t fifoCount;
+uint8_t fifoBuffer[64];
 
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+
+Quaternion rotation;
+VectorFloat gravity;
+float euler[3];
+float ypr[3];
+
+
+int16_t motorRegister[3];
+float target[3];
+bool calibrated = false;
+
 
 void dmpDataReady()
 {
@@ -53,20 +52,195 @@ void dmpDataReady()
 }
 
 
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
-
-bool initialSetup = false;
-
-float initialYaw = 0.0;
-float initialPitch = 0.0;
-float initialRoll = 0.0;
-
-bool center = false;
-
-void initMotors()
+void setup()
 {
+  Wire.begin();
+  Wire.setClock(400000);
+
+  Serial.begin(115200);
+
+  if (initMPU())
+  {
+    sensorThread->onRun(sensorCallback);
+    sensorThread->setInterval(5);
+    
+    mainThread.add(sensorThread);
+  }
+
+  if (initL298N())
+  {
+    motorThread->onRun(motorCallback);
+    motorThread->setInterval(10);
+
+    mainThread.add(motorThread);
+  }
+
+  Serial.println(F("Calibrating . . ."));
+
+  outputThread->onRun(outputCallback);
+  outputThread->setInterval(1000);
+  
+  mainThread.add(outputThread);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+
+void loop()
+{
+  mainThread.run();
+}
+
+
+void outputCallback()
+{
+  if (!calibrated) return;
+  
+  Serial.println();
+  
+  /*Serial.print(F("Euler\t"));
+  Serial.print(euler[0] * 180 / M_PI);
+  Serial.print(F("\t"));
+  Serial.print(euler[1] * 180 / M_PI);
+  Serial.print(F("\t"));
+  Serial.println(euler[2] * 180 / M_PI);*/
+
+  Serial.print(F("YPR\t"));
+  Serial.print(ypr[0] * 180 / M_PI);
+  Serial.print(F("\t"));
+  Serial.print(ypr[1] * 180 / M_PI);
+  Serial.print(F("\t"));
+  Serial.println(ypr[2] * 180 / M_PI);
+
+  switch (motorRegister[0])
+  {
+    case 1:
+      Serial.print(F("Forward: "));
+      Serial.println(motorRegister[1]);
+      
+      break;
+    case 0:
+      Serial.print(F("Center: "));
+
+      if (motorRegister[1] == 0)
+        Serial.println(F("IDLE"));
+      else if (motorRegister[1] == 255)
+        Serial.println(F("BRAKING"));
+      
+      break;
+    case -1:
+      Serial.print(F("Backward: "));
+      Serial.println(motorRegister[1]);
+      
+      break;
+  }
+
+  Serial.print(F("Target: "));
+  Serial.print(target[0] * 180 / M_PI);
+  Serial.print(F("\t"));
+  Serial.print(target[1] * 180 / M_PI);
+  Serial.print(F("\t"));
+  Serial.println(target[2] * 180 / M_PI);
+
+  //Serial.println(motorRegister[0]);
+  //Serial.println(motorRegister[1]);
+}
+
+
+void sensorCallback()
+{
+  if (!mpuInterrupt) return;
+
+  mpuInterrupt = false;
+  
+  mpuIntStatus = mpu.getIntStatus();
+  fifoCount = mpu.getFIFOCount();
+
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024)
+    mpu.resetFIFO();
+  else if (mpuIntStatus & 0x02)
+  {
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    fifoCount -= packetSize;
+
+    mpu.dmpGetQuaternion(&rotation, fifoBuffer);
+    mpu.dmpGetEuler(euler, &rotation);
+    mpu.dmpGetGravity(&gravity, &rotation);
+    mpu.dmpGetYawPitchRoll(ypr, &rotation, &gravity);
+  }
+}
+
+
+void motorCallback()
+{
+  if (millis() / 1000 < 10 && !calibrated)
+  {
+    target[0] = (target[0] + ypr[0]) / 2;
+    target[1] = (target[1] + ypr[1]) / 2;
+    target[2] = (target[2] + ypr[2]) / 2;
+  }
+  else if (!calibrated)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    
+    calibrated = true;
+  }
+  else
+  {
+    double targetAngle = (ypr[2] - target[2]) * 255 / M_PI;
+
+    if (abs(targetAngle) > 0 && abs(targetAngle) < 1)
+    {
+      /* Ugly hack to "force" an autonomous erection */
+      target[0] = (target[0] + ypr[0]) / 3;
+      target[1] = (target[1] + ypr[1]) / 3;
+      target[2] = (target[2] + ypr[2]) / 3;
+    }
+    
+    motorControl(targetAngle);
+  }
+}
+
+
+bool initMPU()
+{
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  if (mpu.testConnection())
+  {
+    if (mpu.dmpInitialize() == 0)
+    {
+      mpu.setXAccelOffset(150);
+      mpu.setYAccelOffset(-1185);
+      mpu.setZAccelOffset(1138);
+      
+      mpu.setXGyroOffset(0);
+      mpu.setYGyroOffset(0);
+      mpu.setZGyroOffset(0);
+
+      mpu.setDMPEnabled(true);
+      attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+      mpuIntStatus = mpu.getIntStatus();
+      packetSize = mpu.dmpGetFIFOPacketSize();
+
+      return true;
+    }
+  }
+
+  return false;
+}
+
+
+bool initL298N()
+{
+  motorRegister[0] = 0;
+  motorRegister[1] = 0;
+  motorRegister[2] = 0;
+  
   pinMode(ENA, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -79,244 +253,66 @@ void initMotors()
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, HIGH);
-}
 
-void setup()
-{
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-  Wire.begin();
-  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-  Fastwire::setup(400, true);
-#endif
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
 
-  // initialize serial communication
-  // (115200 chosen because it is required for Teapot Demo output, but it's
-  // really up to you depending on your project)
-  Serial.begin(115200);
-  while (!Serial); // wait for Leonardo enumeration, others continue immediately
-
-  // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
-  // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
-  // the baud timing being too misaligned with processor ticks. You must use
-  // 38400 or slower in these cases, or use some kind of external separate
-  // crystal solution for the UART timer.
-
-  // initialize device
-  Serial.println(F("Initializing I2C devices..."));
-  mpu.initialize();
-  pinMode(INTERRUPT_PIN, INPUT);
-
-  // verify connection
-  Serial.println(F("Testing device connections..."));
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-  // load and configure the DMP
-  Serial.println(F("Initializing DMP..."));
-  devStatus = mpu.dmpInitialize();
-
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(220);
-  mpu.setYGyroOffset(76);
-  mpu.setZGyroOffset(-85);
-  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
-
-  // make sure it worked (returns 0 if so)
-  if (devStatus == 0)
-  {
-    // turn on the DMP, now that it's ready
-    Serial.println(F("Enabling DMP..."));
-    mpu.setDMPEnabled(true);
-
-    // enable Arduino interrupt detection
-    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
-
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
-    Serial.println(F("DMP ready!"));
-    dmpReady = true;
-
-    // get expected DMP packet size for later comparison
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  }
-  else
-  {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-    Serial.print(F("DMP Initialization failed (code "));
-    Serial.print(devStatus);
-    Serial.println(F(")"));
-  }
-
-  // configure LED for output
-  pinMode(LED_PIN, OUTPUT);
-
-  initMotors();
+  return true;
 }
 
 
 void motorControl(double out)
 {
-  // forward
-  if (out > 2)
-  {
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
-
-    center = false;
-
-#ifdef OUTPUT_READABLE_MOTOR
-    Serial.print("Forward: ");
-#endif
-  }
-  // backward
-  else if (out < -2)
-  {
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-
-    center = false;
-    
-#ifdef OUTPUT_READABLE_MOTOR
-    Serial.print("Backward: ");
-#endif
-  }
+  if (out >= 15)
+    motorRegister[0] = 1;
+  else if (out > -15 && out < 15)
+    motorRegister[0] = 0;
   else
-  {
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, HIGH);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, HIGH);
-
-    center = true;
-    
-#ifdef OUTPUT_READABLE_MOTOR
-    Serial.print("Center: ");
-#endif
-  }
+    motorRegister[0] = -1;
 
   int velocity = abs(out);
+  
+  motorRegister[1] = (velocity > 5) ? (velocity < 15) ? 255 : velocity * M_PI : 0;
 
-  if (velocity < 0) velocity = 0;
+  switch (motorRegister[0])
+  {
+    case 1:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
 
-  velocity = velocity * velocity;
+      break;
+    case 0:
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
 
-  if (center && velocity > 0) velocity = 255;
-  if (velocity > 255) velocity = 255;
+      if (motorRegister[1] == 255 && motorRegister[2] < 20)
+      {
+        motorRegister[2]++;
 
-  byte velocityOut = abs(velocity);
+        if (motorRegister[2] >= 20)
+          motorRegister[2] = 0;
+        else if (motorRegister[2] >= 10)
+          motorRegister[1] = 0;
+      }
+      
+      break;
+    case -1:
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, HIGH);
 
-  analogWrite(ENA, velocityOut);
-  analogWrite(ENB, velocityOut);
+      break;
+  }
 
-#ifdef OUTPUT_READABLE_MOTOR
-  Serial.println(velocity);
-#endif
+  if (motorRegister[1] > 255)
+    motorRegister[1] = 255;
+
+  analogWrite(ENA, motorRegister[1]);
+  analogWrite(ENB, motorRegister[1]);
 }
 
-
-// ================================================================
-// ===                    MAIN PROGRAM LOOP                     ===
-// ================================================================
-
-void loop()
-{
-  // if programming failed, don't try to do anything
-  if (!dmpReady) return;
-
-  // wait for MPU interrupt or extra packet(s) available
-  while (!mpuInterrupt && fifoCount < packetSize)
-  {
-    // other program behavior stuff here
-    // .
-    // .
-    // .
-    // if you are really paranoid you can frequently test in between other
-    // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-    // while() loop to immediately process the MPU data
-    // .
-    // .
-    // .
-  }
-
-  // reset interrupt flag and get INT_STATUS byte
-  mpuInterrupt = false;
-  mpuIntStatus = mpu.getIntStatus();
-
-  // get current FIFO count
-  fifoCount = mpu.getFIFOCount();
-
-  // check for overflow (this should never happen unless our code is too inefficient)
-  if ((mpuIntStatus & 0x10) || fifoCount == 1024)
-  {
-    // reset so we can continue cleanly
-    mpu.resetFIFO();
-    Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-  }
-  else if (mpuIntStatus & 0x02)
-  {
-    // wait for correct available data length, should be a VERY short wait
-    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-    // read a packet from FIFO
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-    // track FIFO count here in case there is > 1 packet available
-    // (this lets us immediately read more without waiting for an interrupt)
-    fifoCount -= packetSize;
-
-    // display Euler angles in degrees
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-
-    if (!initialSetup)
-    {
-      initialYaw = ypr[0] * 180 / M_PI;
-      initialPitch = ypr[1] * 180 / M_PI;
-      initialRoll = ypr[2] * 180 / M_PI;
-
-      initialSetup = true;
-    }
-
-#ifdef OUTPUT_READABLE_YAWPITCHROLL
-    Serial.print("ypr\t");
-    Serial.print(ypr[0] * 180 / M_PI);
-    Serial.print("\t");
-    Serial.print(ypr[1] * 180 / M_PI);
-    Serial.print("\t");
-    Serial.println(ypr[2] * 180 / M_PI);
-#endif
-
-#ifdef OUTPUT_TEAPOT
-    // display quaternion values in InvenSense Teapot demo format:
-    teapotPacket[2] = fifoBuffer[0];
-    teapotPacket[3] = fifoBuffer[1];
-    teapotPacket[4] = fifoBuffer[4];
-    teapotPacket[5] = fifoBuffer[5];
-    teapotPacket[6] = fifoBuffer[8];
-    teapotPacket[7] = fifoBuffer[9];
-    teapotPacket[8] = fifoBuffer[12];
-    teapotPacket[9] = fifoBuffer[13];
-    Serial.write(teapotPacket, 14);
-    teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
-#endif
-
-    float correction = (ypr[2] * 180 / M_PI) - initialRoll;
-    motorControl(correction);
-
-    // blink LED to indicate activity
-    blinkState = !blinkState;
-    digitalWrite(LED_PIN, blinkState);
-  }
-}
